34896e848ccb97c438a49cf6e0475e6a2386fc70
shivenhanda <shivenhanda2003@gmail.com>
2026-03-09 20:13:43 +0530

    feat: Add localStorage to Save Data

--- frontend/src/AddTask.jsx
+++ frontend/src/AddTask.jsx
@@ -5,18 +5,33 @@ export default function AddTask(){
         register,
         handleSubmit,
         watch,
-        formState:{errors},
+        formState:{errors,isSubmitting},
     }=useForm();
+
+    async function saveTask(data){
+        let dataList=JSON.parse(localStorage.getItem("saveTask")) || [];
+        dataList.push(data)
+        localStorage.setItem("saveTask",JSON.stringify(dataList))
+    }
     return(
         <div style={{display:"flex",flexDirection:"column",gap:"10px"
         }}>
         <h2 style={{textAlign:"center"}}>Add Task</h2>
-        <form onSubmit={handleSubmit(data=>console.log(data))}>
+        <form onSubmit={handleSubmit(saveTask)}>
             <label htmlFor="task">Enter Task Name</label>
             <input {...register ('task',{required:true,minLength:{value:3,message:"Minimum Length 3 characters"}})} id="task"/>
             {
                 errors.task && <p>{errors.task.message}</p>
             }
+            <label htmlFor="date">Enter Task Date</label>
+            <input type="date" value={new Date().toISOString().split("T")[0]} min={new Date().toISOString().split("T")[0]} {...register ('date',{required:true})} id="date"/>
+            <label htmlFor="tag">Enter Task Tag</label>
+            <input {...register ('tag')} id="tag"/>
+            <select name="priority" id="priority">
+                    <option value="High Priority">High Priority</option>
+                    <option value="Medium Priority">Medium Priority</option>
+                    <option value="Low Priority">Low Priority</option>
+                </select>
             <button>Submit</button>
         </form>
         </div>
